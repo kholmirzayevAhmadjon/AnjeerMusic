24c33c6bbc0326388316ac5e74599bdadecb664f
Ahmadjon <xolmirzayevahmadjon877@gmail.com>
2024-03-11 11:15:27 +0500

    Create AnjeerMusic.Domain

---
+++ AnjeerMusic.Domain/Musics/Music.cs
@@ -0,0 +1,10 @@
+﻿using AnjeerMusic.Domain.Commons;
+
+namespace AnjeerMusic.Domain.Musics;
+
+public class Music : Auditable
+{
+    public string Name { get; set; }
+
+    public byte[] MusicData { get; set; }
+}
---
+++ AnjeerMusic.Domain/Users/User.cs
@@ -0,0 +1,14 @@
+﻿using AnjeerMusic.Domain.Commons;
+
+namespace AnjeerMusic.Domain.Users;
+
+public class User : Auditable
+{
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
+    public long ChatId { get; set; }
+
+    public long PhoneNumber { get; set; }
+}
