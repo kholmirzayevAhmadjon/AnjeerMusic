42c7886ac47652d95eb53939b4d5f930d178fb5a
Javohir <xursanboyevj26@gmail.com>
2024-03-11 15:41:46 +0500

    Added Database Connection , Create DbContext Tables

---
+++ AnjeerMusic.Data/Migrations/20240311103838_Added6.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace AnjeerMusic.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Added6 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "musicals",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    MusicData = table.Column<byte[]>(type: "bytea", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    DeletedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_musicals", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    FirstName = table.Column<string>(type: "text", nullable: true),
+                    LastName = table.Column<string>(type: "text", nullable: true),
+                    ChatId = table.Column<long>(type: "bigint", nullable: false),
+                    PhoneNumber = table.Column<long>(type: "bigint", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    DeletedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "userMusicals",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UserId = table.Column<long>(type: "bigint", nullable: false),
+                    MusicId = table.Column<long>(type: "bigint", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    DeletedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_userMusicals", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_userMusicals_musicals_MusicId",
+                        column: x => x.MusicId,
+                        principalTable: "musicals",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_userMusicals_users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_userMusicals_MusicId",
+                table: "userMusicals",
+                column: "MusicId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_userMusicals_UserId",
+                table: "userMusicals",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "userMusicals");
+
+            migrationBuilder.DropTable(
+                name: "musicals");
+
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
--- AnjeerMusic.Domain/UserMusics/UserMusic.cs
+++ AnjeerMusic.Domain/UserMusics/UserMusic.cs
@@ -1,10 +1,13 @@
 ﻿using AnjeerMusic.Domain.Commons;
+using AnjeerMusic.Domain.Musics;
+using AnjeerMusic.Domain.Users;
 
 namespace AnjeerMusic.Domain.UserMusics;
 
 public class UserMusic : Auditable
 {
     public long UserId { get; set; }
-
+    public User User { get; set; }
     public long MusicId { get; set; }
+    public Music Music { get; set; }
 }
