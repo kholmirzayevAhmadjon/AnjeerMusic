c192a5d5e3494639a5f63092eb5aadd379e60a6c
Javohir <xursanboyevj26@gmail.com>
2024-03-12 16:30:09 +0500

    Checking TgBotService

--- AnjeerMusic.Domain/Configurations/Constants.cs
+++ AnjeerMusic.Domain/Configurations/Constants.cs
@@ -4,4 +4,6 @@ public class Constants
 {
     public const string ConnectionString = "Server=dpg-cngviua0si5c73blm71g-a.oregon-postgres.render.com;Database=airportdb;" +
      "User Id=javohir03;Password=cIKLeyAx4NGBPfMujI252EpIkCSsySPq;";
+
+    public const string token = "6991402457:AAH9iq8LHWqj9s5yt8IJSTJbye9Zs6Yr9fA";
 }
--- AnjeerMusic.Service/Service/TgBotService.cs
+++ AnjeerMusic.Service/Service/TgBotService.cs
@@ -2,15 +2,18 @@
 using Telegram.Bot.Types.Enums;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.ReplyMarkups;
+using AnjeerMusic.Models.UserModels;
 
 namespace AnjeerMusic.Service.Service;
 
 public class TgBotService
 {
     private TelegramBotClient client;
-    public TgBotService(TelegramBotClient client)
+    private UserService userService;
+    public TgBotService(TelegramBotClient client,UserService userService)
     {
         this.client = client;
+        this.userService = userService;
     }
 
     public async Task Run()
@@ -31,7 +34,17 @@ public class TgBotService
 
     public async Task UserCreateAsync(Message message)
     {
-           
+        UserCreationModel user = new UserCreationModel()
+        {
+            ChatId = message.Chat.Id,
+            FirstName = message.Chat.FirstName,
+            LastName = message.Chat.LastName
+        };
+
+        await client.SendTextMessageAsync(message.Chat.Id, $"Hi {message?.Chat?.FirstName?.ToString()}, " +
+               $"Welcome to our Free Cloud Memeory");
+
+        await userService.CreateAsync(user);
     }
 
     public async Task Error(ITelegramBotClient client, Exception exception, CancellationToken token)
