cbfd7c07ae0055dac262922d3c62da4daf779656
Ahmadjon <xolmirzayevahmadjon877@gmail.com>
2024-03-12 15:25:46 +0500

    Added UserCreateMetod

--- AnjeerMusic.Service/Interface/IUserService.cs
+++ AnjeerMusic.Service/Interface/IUserService.cs
@@ -1,7 +1,16 @@
-﻿namespace AnjeerMusic.Service.Interface;
+﻿using AnjeerMusic.Domain.Users;
+using AnjeerMusic.Models.UserModels;
+
+namespace AnjeerMusic.Service.Interface;
 
 public interface IUserService
 {
+    Task<UserViewModel> CreateAsync(UserCreationModel user);
 
+    Task<UserViewModel> UpdateAsync(long id, UserUpdateModel user);
+
+    Task<bool> DeleteAsync(long id);
+
+    Task<UserViewModel> GetByIdAsync(long id);
 }
 
---
+++ AnjeerMusic.Service/Service/UserService.cs
@@ -0,0 +1,42 @@
+﻿using AnjeerMusic.Data.Repositories;
+using AnjeerMusic.Domain.Users;
+using AnjeerMusic.Models.UserModels;
+using AnjeerMusic.Service.Extensions;
+using AnjeerMusic.Service.Interface;
+
+namespace AnjeerMusic.Service.Service;
+
+public class UserService : IUserService
+{
+    private readonly Repository<User> repository;
+    public UserService(Repository<User> repository)
+    {
+        this.repository = repository;
+    }
+
+    public async Task<UserViewModel> CreateAsync(UserCreationModel user)
+    {
+        var users = repository.SelectAllAsQueryable();
+        var existUser = users.FirstOrDefault(u => u.ChatId == user.ChatId);
+        if (existUser != null)
+            return await UpdateAsync(existUser.Id, user.MapTo<UserUpdateModel>());
+
+        var createUser = await repository.InsertAsync(user.MapTo<User>());
+        return createUser.MapTo<UserViewModel>();
+    }
+
+    public Task<bool> DeleteAsync(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<UserViewModel> GetByIdAsync(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<UserViewModel> UpdateAsync(long id, UserUpdateModel user)
+    {
+        throw new NotImplementedException();
+    }
+}
