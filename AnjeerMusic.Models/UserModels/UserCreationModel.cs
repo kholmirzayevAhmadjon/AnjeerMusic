c11b0d1b05563cae3660ce505337a98b41f3c7c4
Javohir <xursanboyevj26@gmail.com>
2024-03-12 12:17:24 +0500

    Added DTO User Model

---
+++ AnjeerMusic.Models/UserModels/UserCreationModel.cs
@@ -0,0 +1,12 @@
+﻿namespace AnjeerMusic.Models.UserModels;
+
+public class UserCreationModel
+{
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
+    public long ChatId { get; set; }
+
+    public long PhoneNumber { get; set; }
+}
---
+++ AnjeerMusic.Models/UserModels/UserViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace AnjeerMusic.Models.UserModels;
+
+public class UserViewModel
+{
+    public long Id { get; set; }
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
+    public long ChatId { get; set; }
+
+    public long PhoneNumber { get; set; }
+}
