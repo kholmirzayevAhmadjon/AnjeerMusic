b4b327ed6d11bf6c675a5bb1cfeb8c241acf9d6f
Javohir <xursanboyevj26@gmail.com>
2024-03-13 12:54:55 +0500

    Added UserMusic Functions

--- AnjeerMusic.Service/Interface/IUserMusicService.cs
+++ AnjeerMusic.Service/Interface/IUserMusicService.cs
@@ -1,14 +1,10 @@
-﻿using AnjeerMusic.Models.UserModels;
-using AnjeerMusic.Models.UserMusicModels;
+﻿using AnjeerMusic.Models.UserMusicModels;
 
 namespace AnjeerMusic.Service.Interface;
 
 public interface IUserMusicService
 {
-    Task<UserMusicViewModels> CreateAsync(UserCreationModel model);
-
-    Task<UserMusicViewModels> UpdateAsync(long id, UserMusicViewModels model);
+    Task<UserMusicViewModels> CreateAsync(UserMusicCreationModels model);
 
     Task<IEnumerable<UserMusicViewModels>> GetAllAsync(long userId);
 }
-
